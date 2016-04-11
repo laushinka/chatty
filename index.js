0df13a972e649f8e77feccd4c1ece4a64e60a0f8
laushinka <laurie.malau@gmail.com>
2016-04-11 16:33:03 +0700

    Add socket.io

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){ //Listen on the connection event for incoming sockets, and log it to the console
+    console.log('a user connected')
+});
+
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+    console.log('Hello, there! Let\'s listen on :3000');
 });
