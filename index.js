f5055d3bc00ddaf59423b0d6b32cd1f91ea22fcd
laushinka <laurie.malau@gmail.com>
2016-04-11 12:33:19 +0700

    Move index.js, add index.html, refactor route handler in index.js to use sendFile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello World</h1>');
+    res.sendFile('<h1>Hello World</h1>');
 });
 
 http.listen(3000, function(){
